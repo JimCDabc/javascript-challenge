c595c119e40cfff68bb896f0eb0ef2da5c6b0ca6
JimCDabc <jim.comas@gmail.com>
2020-01-28 22:16:03 -0600

    Initial UFO-level-2 based on UFO-level-1

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,76 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// Console.log the data from data.js
+// console.log(tableData);
+
+// function loop through each report and add to the table
+function buildHTMLTable(reports) {
+  // Get a reference to the table body
+  var tbody = d3.select("tbody");
+  // Clear table body
+  tbody.html("")
+
+  // loop through ech report and add to HTML table body
+  reports.forEach((report) => {
+    console.log(report)
+    var row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+// build initial table from the data
+buildHTMLTable(tableData);
+
+// // filter by date function
+// function filterByDate(report, inputDate) {
+//   var parseDate = d3.timeParse("%m/%d/%Y");
+//   var rdate = parseDate(report.datetime);
+//   var idate = parseDate("1/13/2010")
+//   console.log(`report date time ${report.datetime}`);
+
+//   console.log(` input date: ${idate}`);
+//   console.log(`report date: ${rdate}`);
+//   console.log(rdate.getTime() !== idate.getTime());
+//   return(rdate.getTime() === idate.getTime());
+// }
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // initialize filterdData array
+  //var filteredData = [];
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputDateStr = inputElement.property("value");
+
+  // parse the date and check to see if valid date
+  var parseDate = d3.timeParse("%m/%d/%Y");
+  var inputDate = parseDate(inputDateStr);
+ 
+  if (inputDate !== null) {
+    console.log(`${inputDateStr} parsed to datetime: ${inputDate}`);
+
+    // filter table by date-time that is taken from inputvalue.
+    // var filteredData = tableData.filter(report => (parseDate(report.datetime) == inputDate));
+    var filteredData = 
+      tableData.filter(report => (parseDate(report.datetime).getTime() === inputDate.getTime()));
+
+    console.log(filteredData);
+    buildHTMLTable(filteredData);
+    
+  } else {
+    console.log(`INPUT ERROR: Invalid date entered: ${inputDateStr}`)
+    buildHTMLTable([]);
+  }
+
+});
